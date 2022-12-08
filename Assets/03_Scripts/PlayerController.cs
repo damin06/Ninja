aeb0173b88a839b564eb46261bad640b2b374e55
damin06 <ko060614@gmail.com>
2022-12-08 15:41:00 +0900

    .

--- Assets/03_Scripts/PlayerController.cs
+++ Assets/03_Scripts/PlayerController.cs
@@ -1,18 +1,46 @@
+using System.Runtime.InteropServices;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Player")]
+    [SerializeField] private float Speed;
+    private float currenSpeed;
+
+    private Rigidbody2D rb;
+    private Animator ani;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+        currenSpeed = Speed;
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0 || vertical != 0)
+        {
+
+            if (currenSpeed < 10)
+            {
+                currenSpeed += Time.deltaTime;
+            }
+        }
+        else
+        {
+            currenSpeed = Speed;
+        }
+        rb.velocity = new Vector3(horizontal, vertical, 0).normalized * currenSpeed;
+        ani.SetFloat("inputX", horizontal);
+        ani.SetFloat("inputY", vertical);
     }
+
+
 }
